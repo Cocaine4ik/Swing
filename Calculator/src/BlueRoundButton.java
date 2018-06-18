ac74edf1e682f89b6cd4d58be60b2896f85042c5
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-18 17:55:31 +0300

    class which extends RoundButton with new styles

---
+++ Calculator/src/BlueRoundButton.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class BlueRoundButton extends RoundButton {
+
+	public BlueRoundButton(String label) {
+		super(label);
+		// TODO Auto-generated constructor stub
+		setBackground(new Color(4, 178, 217));
+		setForeground(new Color(255, 255, 255));
+	}
+
+}
