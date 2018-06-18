9260358d2dfa195a098bda56e8aff0a431f7800e
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-18 12:54:24 +0300

    added background color

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,14 +1,18 @@
 import java.awt.*;
+import java.awt.font.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class Calculator implements ActionListener{
+public class Calculator implements ActionListener, KeyListener{
 
+	
 	static final int n = 4;
 	static final int k = 6;
 	
-	JLabel screen;
+	String textField= "";
+	
+	JTextField screen;
 	
 	JButton AC, negeativeAndPositive, percent,
 	division, multiplication, minus, plus, equal;
@@ -20,15 +24,43 @@ public class Calculator implements ActionListener{
 		
 		JFrame frame = new JFrame("Calculator");
 		frame.setLayout(new GridLayout(n, k));
-		frame.setSize(200, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 400);
+		frame.getContentPane().setBackground(new Color(33, 35, 36));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		
+		frame.setVisible(true);
 		
+		screen = new JTextField(textField);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Calculator();
+			}
+		});
 	}
 }
